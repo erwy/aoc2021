134f61a51a231006ed2dabf5cf25c222dd1437b0
Erik Wyke <erik.wyke@gmail.com>
2021-12-02 06:26:09 +0100

    Day2

--- app/src/main/kotlin/aoc2021/day2/Part1.kt
+++ app/src/main/kotlin/aoc2021/day2/Part1.kt
@@ -6,6 +6,19 @@ import aoc2021.utils.Part
 class Part1(private val input: List<String>) : Part{
 
     override fun run(): Long {
-        return 0
+        val pairs = input.map { it.split(" ") }
+            .map { Pair(it[0], it[1].toLong()) }
+
+        var depth = 0L
+        var horizontal = 0L
+
+        pairs.forEach {
+            when(it.first) {
+                "forward" -> horizontal+=it.second
+                "up" -> depth-=it.second
+                "down" -> depth+=it.second
+            }
+        }
+        return depth*horizontal
     }
 }
--- app/src/main/kotlin/aoc2021/day2/Part2.kt
+++ app/src/main/kotlin/aoc2021/day2/Part2.kt
@@ -4,6 +4,28 @@ import aoc2021.utils.Part
 
 class Part2(private val input: List<String>) : Part{
     override fun run(): Long {
-        return 0
+        val pairs = input.map { it.split(" ") }
+            .map { Pair(it[0], it[1].toLong()) }
+
+        var depth = 0L
+        var horizontal = 0L
+        var aim = 0L
+        pairs.forEach {
+            when(it.first) {
+                "forward" -> {
+                    horizontal+=it.second
+                    depth+=(aim*it.second)
+                }
+                "up" -> {
+                    //depth-=it.second
+                    aim-=it.second
+                }
+                "down" -> {
+                    //depth+=it.second
+                    aim+=it.second
+                }
+            }
+        }
+        return depth*horizontal
     }
 }
