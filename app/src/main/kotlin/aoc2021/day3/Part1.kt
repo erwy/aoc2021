0dbb97f266a03a7735ebacd3677db7376ea16e5a
Erik Wyke <erik.wyke@gmail.com>
2021-12-03 07:24:18 +0100

    Wip day 3

--- app/src/main/kotlin/aoc2021/day3/Part1.kt
+++ app/src/main/kotlin/aoc2021/day3/Part1.kt
@@ -2,22 +2,32 @@ package aoc2021.day3
 
 import aoc2021.utils.Part
 
-class Part1(private val input: List<String>) : Part{
+class Part1(private val input: List<String>) : Part {
 
     override fun run(): Long {
-        val pairs = input.map { it.split(" ") }
-            .map { Pair(it[0], it[1].toLong()) }
-
-        var depth = 0L
-        var horizontal = 0L
-
-        pairs.forEach {
-            when(it.first) {
-                "forward" -> horizontal+=it.second
-                "up" -> depth-=it.second
-                "down" -> depth+=it.second
+        val mapOfIndexToPair = mutableMapOf<Int, Pair<Int, Int>>()
+        input.forEach {
+            it.forEachIndexed { index, c ->
+                val orDefault = mapOfIndexToPair.getOrDefault(index, Pair(0, 0))
+                if (c == '0') {
+                    mapOfIndexToPair[index] = orDefault.copy(orDefault.first + 1, orDefault.second)
+                } else {
+                    mapOfIndexToPair[index] = orDefault.copy(orDefault.first, orDefault.second + 1)
+                }
             }
         }
-        return depth*horizontal
-    }
+        var gamma = ""
+        var epsilon = ""
+        mapOfIndexToPair.values
+            .forEach {
+                if (it.first > it.second) {
+                    gamma += "0"
+                    epsilon += "1"
+                } else {
+                    gamma += "1"
+                    epsilon += "0"
+                }
+            }
+    return Integer.parseInt(gamma,2).toLong() * Integer.parseInt(epsilon,2).toLong()
+}
 }
--- app/src/main/kotlin/aoc2021/day3/Part2.kt
+++ app/src/main/kotlin/aoc2021/day3/Part2.kt
@@ -2,28 +2,64 @@ package aoc2021.day3
 
 import aoc2021.utils.Part
 
-class Part2(private val input: List<String>) : Part{
+class Part2(private val input: List<String>) : Part {
     override fun run(): Long {
-        val pairs = input.map { it.split(" ") }
-            .map { Pair(it[0], it[1].toLong()) }
+        var oxygenStrings = input.toMutableList()
+        val mapOfIndexToPair = indexToPairMap(oxygenStrings)
+        mapOfIndexToPair
+        for () {
 
-        var depth = 0L
-        var horizontal = 0L
-        var aim = 0L
-        pairs.forEach {
-            when(it.first) {
-                "forward" -> {
-                    horizontal+=it.second
-                    depth+=(aim*it.second)
-                }
-                "up" -> {
-                    aim-=it.second
-                }
-                "down" -> {
-                    aim+=it.second
+            // Oxygen
+            mapOfIndexToPair.entries.forEach {
+                if (oxygenStrings.size != 1) {
+                    val index = it.key
+                    val toKeep = if (it.value.first > it.value.second) {
+                        '0'
+                    } else if (it.value.first < it.value.second) {
+                        '1'
+                    } else {
+                        '1'
+                    }
+                    oxygenStrings.removeIf { it[index] != toKeep }
                 }
             }
         }
-        return depth*horizontal
+
+        var co2Strings = input.toMutableList()
+        // C02
+        while(oxygenStrings.size > 1) {
+            val mapOfIndexToPair = indexToPairMap(co2Strings)
+            mapOfIndexToPair.entries.forEach {
+                if (co2Strings.size != 1) {
+                    val index = it.key
+                    val toKeep = if (it.value.first > it.value.second) {
+                        '1'
+                    } else if (it.value.first < it.value.second) {
+                        '0'
+                    } else {
+                        '0'
+                    }
+                    co2Strings.removeIf { it[index] != toKeep }
+                }
+            }
+        }
+
+
+        return Integer.parseInt(oxygenStrings.first(),2).toLong() * Integer.parseInt(co2Strings.first(),2).toLong()
+    }
+
+    private fun indexToPairMap(input: List<String>): MutableMap<Int, Pair<Int, Int>> {
+        val mapOfIndexToPair = mutableMapOf<Int, Pair<Int, Int>>()
+        input.forEach {
+            it.forEachIndexed { index, c ->
+                val orDefault = mapOfIndexToPair.getOrDefault(index, Pair(0, 0))
+                if (c == '0') {
+                    mapOfIndexToPair[index] = orDefault.copy(orDefault.first + 1, orDefault.second)
+                } else {
+                    mapOfIndexToPair[index] = orDefault.copy(orDefault.first, orDefault.second + 1)
+                }
+            }
+        }
+        return mapOfIndexToPair
     }
 }
