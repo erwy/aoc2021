ce7a6149e06487965008ea0e31354403568b8d9b
Erik Wyke <erik.wyke@gmail.com>
2021-12-03 07:37:19 +0100

    Wip day 3

--- app/src/main/kotlin/aoc2021/day3/Part2.kt
+++ app/src/main/kotlin/aoc2021/day3/Part2.kt
@@ -5,47 +5,41 @@ import aoc2021.utils.Part
 class Part2(private val input: List<String>) : Part {
     override fun run(): Long {
         var oxygenStrings = input.toMutableList()
-        val mapOfIndexToPair = indexToPairMap(oxygenStrings)
-        mapOfIndexToPair
-        for () {
-
+        for (index in 0 until indexToPairMap(oxygenStrings).keys.size) {
+            val mapOfIndexToPair = indexToPairMap(oxygenStrings)
             // Oxygen
-            mapOfIndexToPair.entries.forEach {
-                if (oxygenStrings.size != 1) {
-                    val index = it.key
-                    val toKeep = if (it.value.first > it.value.second) {
-                        '0'
-                    } else if (it.value.first < it.value.second) {
-                        '1'
-                    } else {
-                        '1'
-                    }
-                    oxygenStrings.removeIf { it[index] != toKeep }
+            val pair = mapOfIndexToPair[index]
+            if (oxygenStrings.size != 1 && pair != null) {
+                val toKeep = if (pair.first > pair.second) {
+                    '0'
+                } else if (pair.first < pair.second) {
+                    '1'
+                } else {
+                    '1'
                 }
+                oxygenStrings.removeIf { it[index] != toKeep }
             }
         }
 
         var co2Strings = input.toMutableList()
         // C02
-        while(oxygenStrings.size > 1) {
+        for (index in 0 until indexToPairMap(co2Strings).keys.size) {
             val mapOfIndexToPair = indexToPairMap(co2Strings)
-            mapOfIndexToPair.entries.forEach {
-                if (co2Strings.size != 1) {
-                    val index = it.key
-                    val toKeep = if (it.value.first > it.value.second) {
-                        '1'
-                    } else if (it.value.first < it.value.second) {
-                        '0'
-                    } else {
-                        '0'
-                    }
-                    co2Strings.removeIf { it[index] != toKeep }
+            val pair = mapOfIndexToPair[index]
+            if (co2Strings.size != 1 && pair != null) {
+                val toKeep = if (pair.first > pair.second) {
+                    '1'
+                } else if (pair.first < pair.second) {
+                    '0'
+                } else {
+                    '0'
                 }
+                co2Strings.removeIf { it[index] != toKeep }
             }
         }
 
 
-        return Integer.parseInt(oxygenStrings.first(),2).toLong() * Integer.parseInt(co2Strings.first(),2).toLong()
+        return Integer.parseInt(oxygenStrings.first(), 2).toLong() * Integer.parseInt(co2Strings.first(), 2).toLong()
     }
 
     private fun indexToPairMap(input: List<String>): MutableMap<Int, Pair<Int, Int>> {
